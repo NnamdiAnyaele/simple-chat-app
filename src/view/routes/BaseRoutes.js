5229355ab2e5d61aca85af34676d3a44930d3061
Anyaele Nnamdi <nnamdianya2011@gmail.com>
2022-09-20 14:06:31 +0100

    feat: setup routing for dashboard
    setup routing for dashboard

--- src/view/routes/BaseRoutes.js
+++ src/view/routes/BaseRoutes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import { useSelector } from "react-redux";
 
 import Login from "../Pages/Login";
+import Dashboard from "../Pages/Dashboard";
 
 const BaseRoute = () => {
 	// const { user } = useSelector((state) => state.auth);
